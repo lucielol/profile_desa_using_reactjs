86de27f32ee52acc828a28ea55a1e22b1d870902
sp0ng3808 <tediar1337@gmail.com>
2024-05-17 15:49:06 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import { NewsPage } from "./pages/NewsPage";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" exact element={<HomePage/>} />
         <Route path="/news/:id" element={<NewsPage/>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
